fc98716a63daf4c8ad6ba25d8654bd6f931e40d3
Pat <patrick.neel@colorado.edu>
2024-10-09 15:48:46 -0600

    fix create article learn to spell permit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(title: "...", body: "...")
+    @article = Article.new(article_params)
 
     if @article.save
       redirect_to @article
@@ -21,8 +21,22 @@ class ArticlesController < ApplicationController
     end
   end
 
-  private 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
     def article_params
-      params.require(:article).premit(:title, :body)
+      params.require(:article).permit(:title, :body)
     end
 end
